8ab4f64ea7d6f9e67cd3c7a30959943efaab9911
gaharoe <153198261+gaharoe@users.noreply.github.com>
2025-10-23 19:15:43 +0700

    deploy

--- installer.cmd
+++ installer.cmd
@@ -1,5 +1,5 @@
-winget install OpenJS.NodeJS Git.Git --accept-source-agreement
+winget install OpenJS.NodeJS Git.Git --accept-source-agreements --accept-package-agreements
 cd C:\Users\%USERNAME%\Documents
-git clone https://github.com/gaharoe/angkringan-gen-z
-cd angkringan-gen-z
+git clone https://github.com/gaharoe/angkringan
+cd angkringan
 start.cmd
